9aec8aca2d505a0cd9d0e902ae8f855b0d51aab4
Chris Zuber <shgysk8zer0@gmail.com>
2019-06-27 15:12:42 -0700

    Initial commit

--- js/index.js
+++ js/index.js
@@ -1,46 +1,114 @@
 import 'https://cdn.kernvalley.us/js/std-js/deprefixer.js';
 import 'https://cdn.kernvalley.us/js/std-js/shims.js';
-import './share-button.js';
-import './current-year.js';
-import './gravatar-img.js';
-import './imgur-img.js';
-import {$, ready, registerServiceWorker} from 'https://cdn.kernvalley.us/js/std-js/functions.js';
+import 'https://cdn.kernvalley.us/js/std-js/asyncDialog.js';
+import {ready, $} from 'https://cdn.kernvalley.us/js/std-js/functions.js';
 
-if (document.documentElement.dataset.hasOwnProperty('serviceWorker')) {
-	registerServiceWorker(document.documentElement.dataset.serviceWorker).catch(console.error);
-}
-
-document.documentElement.classList.replace('no-js', 'js');
-document.body.classList.toggle('no-dialog', document.createElement('dialog') instanceof HTMLUnknownElement);
-document.body.classList.toggle('no-details', document.createElement('details') instanceof HTMLUnknownElement);
+document.documentElement.classList.toggle('no-dialog', document.createElement('dialog') instanceof HTMLUnknownElement);
 
 ready().then(async () => {
-	$('[data-scroll-to]').click(event => {
-		const target = document.querySelector(event.target.closest('[data-scroll-to]').dataset.scrollTo);
-		target.scrollIntoView({
-			bahavior: 'smooth',
-			block: 'start',
+	async function getCoords({
+		enableHighAccuracy = true,
+		timeout = Infinity,
+		maximumAge = 3000,
+		max = 10,
+	} = {}) {
+		return new Promise((resolve, reject) => {
+			let entries = [];
+			let value = 0;
+			$('#progress').attr({max, value: null});
+			const pid = navigator.geolocation.watchPosition(({coords, timestamp}) => {
+				$('#progress').attr({value});
+				const {
+					longitude,
+					latitude,
+					altitude = null,
+					heading = null,
+					accuracy = null,
+					speed = null,
+				} = coords;
+				const dtime = new Date(timestamp);
+
+				entries.push({
+					longitude,
+					latitude,
+					altitude,
+					heading,
+					accuracy,
+					speed,
+					dtime,
+				});
+
+				if (++value > max) {
+					navigator.geolocation.clearWatch(pid);
+					resolve(entries)
+				}
+			}, err => reject(err), {enableHighAccuracy, timeout, maximumAge});
 		});
-	});
+	}
 
-	$('[data-show]').click(event => {
-		const target = document.querySelector(event.target.closest('[data-show]').dataset.show);
-		if (target instanceof HTMLElement) {
-			target.show();
-		}
-	});
+	$('#button').click(async event => {
+		event.target.disabled = true;
+		$('.error').text('');
 
-	$('[data-show-modal]').click(event => {
-		const target = document.querySelector(event.target.closest('[data-show-modal]').dataset.showModal);
-		if (target instanceof HTMLElement) {
-			target.showModal();
-		}
-	});
+		try {
+			$('#progress-dialog').showModal();
+			$('#table > tbody > tr').remove();
+			$('#table').hide();
 
-	$('[data-close]').click(event => {
-		const target = document.querySelector(event.target.closest('[data-close]').dataset.close);
-		if (target instanceof HTMLElement) {
-			target.tagName === 'DIALOG' ? target.close() : target.open = false;
+			const tmp = document.getElementById('row-template').content;
+			const table = document.getElementById('table');
+			const tbody = table.tBodies.item(0);
+			const entries = await getCoords();
+			[...tbody.rows].forEach(row => row.remove());
+
+			const rows = entries.map(({
+				longitude,
+				latitude,
+				altitude = null,
+				heading = null,
+				speed = null,
+				accuracy = null,
+				dtime = null,
+			}) => {
+				const template = tmp.cloneNode(true);
+				const m2ft = m => m / 3.28084;
+				const mps2mph = v => v / 2.236936;
+
+				$('[data-field="longitude"]', template).text(longitude);
+				$('[data-field="latitude"]', template).text(latitude);
+
+				if (typeof altitude === 'number' && ! Number.isNaN(altitude)) {
+					$('[data-field="altitude"]', template).text(`${m2ft(altitude).toFixed(2)} ft`);
+				}
+
+				if (typeof speed === 'number' && ! Number.isNaN(speed)) {
+					$('[data-field="speed"]', template).text(`${mps2mph(speed).toFixed(2)} mph`);
+				}
+
+				if (typeof heading === 'number' && ! Number.isNaN(heading)) {
+					$('[data-field="heading"]', template).text(`${heading.toFixed(2)}°`);
+				}
+
+				if (typeof accuracy === 'number' && ! Number.isNaN(accuracy)) {
+					$('[data-field="accuracy"]', template).text(`${m2ft(accuracy).toFixed(2)} ft`);
+				}
+
+				if (dtime instanceof Date) {
+					$('[data-field="dtime"]', template).text(dtime.toLocaleString());
+					$('[data-field="dtime"]', template).attr({datetime: dtime.toISOString()});
+				}
+
+				return template;
+			});
+
+			tbody.append(...rows);
+			$('#table').unhide();
+		} catch(err) {
+			console.error(err);
+			$('.error').text(err.message);
+		} finally {
+			event.target.disabled = false;
+			$('#progress-dialog').close();
 		}
 	});
 });
