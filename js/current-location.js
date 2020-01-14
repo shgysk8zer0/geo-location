6ae0d56d6f67c33ae54f224e5f5be3b9ed83de75
Chris Zuber <shgysk8zer0@gmail.com>
2020-01-14 12:33:55 -0800

    Rewrite
    This now displays current data and stores travelled path as a downloadable JSON file

---
+++ js/current-location.js
@@ -0,0 +1,210 @@
+const shadows = new WeakMap();
+const paths = new WeakMap();
+const pids = new WeakMap();
+
+async function set(element, name, value, tag = 'span', attrs = {}) {
+	const el = document.createElement(tag);
+	el.slot = name;
+	el.textContent = value;
+	Object.entries(attrs).forEach(([key, value]) => el.setAttribute(key, value));
+	await clearSlot(element, name);
+	element.append(el);
+}
+
+async function getSlot(element, name) {
+	await element.ready;
+	return shadows.get(element).querySelector(`slot[name="${name}"]`);
+}
+
+function get(element, name) {
+	if (shadows.has(element)) {
+		const slot = shadows.get(element).querySelector(`slot[name="${name}"]`);
+		if (slot instanceof HTMLElement) {
+			const nodes = slot.assignedNodes();
+			if (nodes.length === 1) {
+				return nodes[0].textContent;
+			} else {
+				return null;
+			}
+		} else {
+			return null;
+		}
+	}
+}
+
+async function clearSlot(element, name) {
+	const slot = await getSlot(element, name);
+	slot.assignedNodes().forEach(el => el.remove());
+}
+
+customElements.define('current-location', class HTMLCurrentLocationElement extends HTMLElement {
+	constructor() {
+		super();
+		const shadow = this.attachShadow({mode: 'closed'});
+
+		fetch(new URL('./current-location.html', import.meta.url)).then(async resp => {
+			const html = await resp.text();
+			const parser = new DOMParser();
+			const doc = parser.parseFromString(html, 'text/html');
+			shadow.append(...doc.head.children, ...doc.body.children);
+			shadows.set(this, shadow);
+			this.dispatchEvent(new Event('ready'));
+		});
+	}
+
+	async connectedCallback() {
+		await this.ready;
+		const shadow = shadows.get(this);
+		shadow.getElementById('start').addEventListener('click', () => this.start());
+		shadow.getElementById('stop').addEventListener('click', () => this.stop());
+	}
+
+	toJSON() {
+		const {latitude, longitude, altitude, heading, speed, accuracy} = this;
+		return {latitude, longitude, altitude, heading, speed, accuracy};
+	}
+
+	toString() {
+		return `${this.latitude}, ${this.longitude}`;
+	}
+
+	get ready() {
+		return new Promise(resolve => {
+			if (shadows.has(this)) {
+				resolve();
+			} else {
+				this.addEventListener('ready', () => resolve(), {once: true});
+			}
+		});
+	}
+
+	get accuracy() {
+		return parseFloat(get(this, 'accuracy'));
+	}
+
+	set accuracy(val) {
+		set(this, 'accuracy', val);
+	}
+
+	get altitude() {
+		return parseFloat(get(this, 'altitude'));
+	}
+
+	set altitude(val) {
+		set(this, 'altitude', val);
+	}
+
+	get heading() {
+		return parseFloat(get(this, 'heading'));
+	}
+
+	set heading(val) {
+		set(this, 'heading', val);
+	}
+
+	get latitude() {
+		return parseFloat(get(this, 'latitude'));
+	}
+
+	set latitude(val) {
+		set(this, 'latitude', val);
+	}
+
+	get longitude() {
+		return parseFloat(get(this, 'longitude'));
+	}
+
+	set longitude(val) {
+		set(this, 'longitude', val);
+	}
+
+	get speed() {
+		return parseFloat(get(this, 'speed'));
+	}
+
+	set speed(val) {
+		set(this, 'speed', val);
+	}
+
+	async start() {
+		await this.ready;
+
+		if (pids.has(this)) {
+			throw new Error('Already running');
+		} else {
+			const shadow = shadows.get(this);
+			shadow.getElementById('download').classList.add('no-pointer-events');
+			shadow.getElementById('start').disabled = true;
+			shadow.getElementById('stop').disabled = false;
+			paths.set(this, []);
+			const pid = navigator.geolocation.watchPosition(({coords, timestamp}) => {
+				const {
+					longitude,
+					latitude,
+					altitude = null,
+					heading = null,
+					accuracy = null,
+					altitudeAccuracy,
+					speed = null,
+				} = coords;
+
+				paths.get(this).push({coords: {
+					latitude,
+					longitude,
+					altitude,
+					heading,
+					accuracy,
+					altitudeAccuracy,
+					speed,
+				}, timestamp: new Date(timestamp).toISOString()});
+
+				this.latitude = latitude;
+				this.longitude = longitude;
+				this.altitude = altitude;
+				this.heading = heading;
+				this.speed = speed;
+				this.accuracy = accuracy;
+
+			}, console.error, {
+				enableHighAccuracy: true,
+				timeout: Infinity,
+				maximumAge: 3000,
+			});
+
+			pids.set(this, pid);
+		}
+	}
+
+	async stop() {
+		await this.ready;
+		if (pids.has(this)) {
+			const pid = pids.get(this);
+			const now = new Date();
+			const shadow = shadows.get(this);
+			const path = paths.get(this);
+			const blob = new Blob(
+				[JSON.stringify(paths.get(this), null, 4)],
+				{type: 'application/json'}
+			);
+			const download = shadow.getElementById('download');
+
+			if (download.href.startsWith('blob:')) {
+				URL.revokeObjectURL(download.href);
+			}
+			download.href = URL.createObjectURL(blob);
+			download.download = `path-${now.toISOString()}.json`;
+			download.classList.remove('no-pointer-events');
+			navigator.geolocation.clearWatch(pid);
+			paths.delete(this);
+			pids.delete(this);
+			this.latitude = '';
+			this.longitude = '';
+			this.altitude = '';
+			this.heading = '';
+			this.speed = '';
+			this.accuracy = '';
+			shadow.getElementById('stop').disabled = true;
+			shadow.getElementById('start').disabled = false;
+		}
+	}
+});
--- js/index.js
+++ js/index.js
@@ -1,121 +1,12 @@
 import 'https://cdn.kernvalley.us/js/std-js/deprefixer.js';
 import 'https://cdn.kernvalley.us/js/std-js/shims.js';
 import 'https://cdn.kernvalley.us/js/std-js/asyncDialog.js';
-import {ready, $, registerServiceWorker} from 'https://cdn.kernvalley.us/js/std-js/functions.js';
+import './current-location.js';
+import {registerServiceWorker} from 'https://cdn.kernvalley.us/js/std-js/functions.js';
 
+document.documentElement.classList.remove('no-js', 'js');
 document.documentElement.classList.toggle('no-dialog', document.createElement('dialog') instanceof HTMLUnknownElement);
 
 if (document.documentElement.dataset.hasOwnProperty('serviceWorker')) {
 	registerServiceWorker(document.documentElement.dataset.serviceWorker);
 }
-ready().then(async () => {
-	async function getCoords({
-		enableHighAccuracy = true,
-		timeout = Infinity,
-		maximumAge = 3000,
-		max = 10,
-	} = {}) {
-		return new Promise((resolve, reject) => {
-			let entries = [];
-			let value = 0;
-			$('#progress').attr({max, value: null});
-			const pid = navigator.geolocation.watchPosition(({coords, timestamp}) => {
-				$('#progress').attr({value});
-				const {
-					longitude,
-					latitude,
-					altitude = null,
-					heading = null,
-					accuracy = null,
-					speed = null,
-				} = coords;
-				const dtime = new Date(timestamp);
-
-				entries.push({
-					longitude,
-					latitude,
-					altitude,
-					heading,
-					accuracy,
-					speed,
-					dtime,
-				});
-
-				if (++value > max) {
-					navigator.geolocation.clearWatch(pid);
-					resolve(entries);
-				}
-			}, err => reject(err), {enableHighAccuracy, timeout, maximumAge});
-		});
-	}
-
-	$('#button').click(async event => {
-		event.target.disabled = true;
-		$('.error').text('');
-
-		try {
-			$('#progress-dialog').showModal();
-			$('#table > tbody > tr').remove();
-			$('#table').hide();
-
-			const tmp = document.getElementById('row-template').content;
-			const table = document.getElementById('table');
-			const tbody = table.tBodies.item(0);
-			const entries = await getCoords();
-			[...tbody.rows].forEach(row => row.remove());
-
-			const rows = entries.map(({
-				longitude,
-				latitude,
-				altitude = null,
-				heading = null,
-				speed = null,
-				accuracy = null,
-				dtime = null,
-			}) => {
-				const template = tmp.cloneNode(true);
-				const m2ft = m => m * 3.28084;
-				const mps2mph = v => v * 2.236936;
-
-				$('[data-field="longitude"]', template).text(longitude);
-				$('[data-field="latitude"]', template).text(latitude);
-
-				if (typeof altitude === 'number' && ! Number.isNaN(altitude)) {
-					$('[data-field="altitude"]', template).text(`${m2ft(altitude).toFixed(2)} ft`);
-				}
-
-				if (typeof speed === 'number' && ! Number.isNaN(speed)) {
-					$('[data-field="speed"]', template).text(`${mps2mph(speed).toFixed(2)} mph`);
-				}
-
-				if (typeof heading === 'number' && ! Number.isNaN(heading)) {
-					$('[data-field="heading"]', template).text(`${heading.toFixed(2)}°`);
-				}
-
-				if (typeof accuracy === 'number' && ! Number.isNaN(accuracy)) {
-					$('[data-field="accuracy"]', template).text(`${m2ft(accuracy).toFixed(2)} ft`);
-				}
-
-				if (dtime instanceof Date) {
-					$('[data-field="dtime"]', template).text(dtime.toLocaleString());
-					$('[data-field="dtime"]', template).attr({datetime: dtime.toISOString()});
-				}
-
-				return template;
-			});
-
-			tbody.append(...rows);
-			table.hidden = false;
-			table.scrollIntoView({
-				block: 'start',
-				behavior: 'smooth',
-			});
-		} catch(err) {
-			console.error(err);
-			$('.error').text(err.message);
-		} finally {
-			event.target.disabled = false;
-			$('#progress-dialog').close();
-		}
-	});
-});
